6046350824f4c8f3c610f69907b10ec3c42b993e
Adam Martinez <ajm27@pdx.edu>
2022-12-20 13:03:34 -0800

    added data files

--- 17-quiz/17-main.py
+++ 17-quiz/17-main.py
@@ -1,21 +1,23 @@
 # Part 1 - Class Intro
-class User:
-    def __init__(self, user_id, name):
-        self.id = user_id
-        self.name = name
-        self.followers = 0
-        self.following = 0
+# class User:
+#     def __init__(self, user_id, name):
+#         self.id = user_id
+#         self.name = name
+#         self.followers = 0
+#         self.following = 0
+#
+#     def follow(self, user):
+#         user.followers += 1
+#         self.following += 1
+#
+#
+# user_1 = User('001', 'jack')
+# user_2 = User('002', 'fran')
+# user_1.follow(user_2)
+#
+# print(user_1.following)
+# print(user_1.followers)
+# print(user_2.following)
+# print(user_2.followers)
 
-    def follow(self, user):
-        user.followers += 1
-        self.following += 1
-
-
-user_1 = User('001', 'jack')
-user_2 = User('002', 'fran')
-user_1.follow(user_2)
-
-print(user_1.following)
-print(user_1.followers)
-print(user_2.following)
-print(user_2.followers)
+# Project - Quiz Game
