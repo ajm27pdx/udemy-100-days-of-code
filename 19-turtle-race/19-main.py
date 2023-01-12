27c1b9e7228ec36d726aaadd7c01afdec33a001e
Adam Martinez <ajm27@pdx.edu>
2023-01-12 15:58:24 -0800

    etch-a-sketch done

--- 19-turtle-race/19-main.py
+++ 19-turtle-race/19-main.py
@@ -3,11 +3,34 @@ from turtle import Turtle, Screen
 my_turtle = Turtle()
 screen = Screen()
 
+# Etch-a-sketch
+screen.listen()
+screen.exitonclick()
+
 
 def move_forward():
-    my_turtle.forward(10)
+    my_turtle.forward(5)
 
 
-screen.listen()
-screen.onkey(key="space", fun=move_forward)
-screen.exitonclick()
+def move_backwards():
+    my_turtle.backward(5)
+
+
+def turn_cw():
+    my_turtle.right(5)
+
+
+def turn_ccw():
+    my_turtle.left(5)
+
+
+def reset_screen():
+    screen.reset()
+
+
+screen.onkeypress(key="w", fun=move_forward)
+screen.onkeypress(key="s", fun=move_backwards)
+screen.onkeypress(key="a", fun=turn_ccw)
+screen.onkeypress(key="d", fun=turn_cw)
+screen.onkeypress(key="c", fun=reset_screen)
+
