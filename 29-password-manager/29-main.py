f532e87c023f42ad0dd332f1c1f478e936fd462f
Adam Martinez <ajm27@pdx.edu>
2023-04-11 14:00:19 -0700

    password manager updated with search funciton

--- 29-password-manager/29-main.py
+++ 29-password-manager/29-main.py
@@ -4,6 +4,7 @@ from random import randint, choice, shuffle
 import pyperclip
 import json
 DEFAULT_USERNAME = 'ajm27'
+global data
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -30,7 +31,8 @@ def save():
     website = website_entry.get()
     username = username_entry.get()
     password = password_entry.get()
-    new_data = {website: {username: password}}
+    new_data = {website: {'username': username,
+                          'password': password}}
     global data
     # Validate Data
     if website == '' or username == '' or password == '':
@@ -57,6 +59,24 @@ def save():
     password_entry.delete(0, END)
 
 
+def search():
+    global data
+    try:
+        with open('data.json') as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showerror(title='File Not Found', message='Existing data not found.')
+    else:
+        try:
+            website = website_entry.get()
+            username = data[website]['username']
+            password = data[website]['password']
+        except KeyError:
+            messagebox.showerror(title='Website Not Found', message=f'{website} not found in database.')
+        else:
+            messagebox.showinfo(title=f'{website}', message=f"Username: {username}\nPassword:{password}")
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title('Password Manager')
@@ -80,6 +100,7 @@ password_entry = Entry(width=25)
 # Button Setup
 gen_password_btn = Button(text='Generate Password', command=generate_password)
 add_btn = Button(text='Add', width=29, command=save)
+search_btn = Button(text='Search', command=search)
 
 # Grid Layout
 canvas.grid(row=0, column=1)
@@ -88,12 +109,13 @@ website_lbl.grid(row=1, column=0)
 username_lbl.grid(row=2, column=0)
 password_lbl.grid(row=3, column=0)
 
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1)
 username_entry.grid(row=2, column=1, columnspan=2)
 password_entry.grid(row=3, column=1)
 
 gen_password_btn.grid(row=3, column=2)
 add_btn.grid(row=4, column=1, columnspan=2)
+search_btn.grid(row=1, column=2)
 
 website_entry.focus()
 username_entry.insert(0, DEFAULT_USERNAME)
