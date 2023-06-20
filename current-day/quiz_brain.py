d4597f3add7cec5407dde2caf4bb4e51b8ad356d
Adam Martinez <ajm27@pdx.edu>
2023-06-20 11:05:37 -0700

    Day 34 complete

--- current-day/quiz_brain.py
+++ current-day/quiz_brain.py
@@ -1,5 +1,6 @@
 import html
 
+
 class QuizBrain:
 
     def __init__(self, q_list):
@@ -14,16 +15,15 @@ class QuizBrain:
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {html.unescape(self.current_question.text)} (True/False): ")
-        self.check_answer(user_answer)
+        return f"Q.{self.question_number}: {html.unescape(self.current_question.text)} (True/False): "
+        # user_answer = input(f"Q.{self.question_number}: {html.unescape(self.current_question.text)} (True/False): ")
+        # self.check_answer(user_answer)
 
-    def check_answer(self, user_answer):
-        correct_answer = self.current_question.answer
-        if user_answer.lower() == correct_answer.lower():
+    def check_answer(self, user_answer: bool) -> bool:
+        correct_answer = self.current_question.answer.lower() == 'true'
+        if user_answer == correct_answer:
             self.score += 1
-            print("You got it right!")
+            return True
         else:
-            print("That's wrong.")
+            return False
 
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
