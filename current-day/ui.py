a9c693c46ca0d1f45ab68bbebc17573564680697
Adam Martinez <ajm27@pdx.edu>
2023-06-20 10:01:31 -0700

    Logic complete on 34

---
+++ current-day/ui.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+THEME_COLOR = "#375362"
+
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+
+        self.window.mainloop()
