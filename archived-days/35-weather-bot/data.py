c983b96648a9affa38a92b022dcb986a48a7db5c
Adam Martinez <ajm27@pdx.edu>
2023-06-22 09:33:09 -0700

    Finished Day 35 with telegram bot

--- archived-days/35-weather-bot/data.py
+++ archived-days/35-weather-bot/data.py
@@ -1,9 +1,10 @@
 import requests
+from auth import OWM_id
 
 parameters = {
     'lat': 45.5202471,
     'lon': -122.674194,
-    'appid': 'b46251243caf85f31844ccabb8be9891',
+    'appid': OWM_id,
     'exclude': 'current,minutely,daily'
 }
 #https://api.openweathermap.org/data/2.8/onecall?lat=45.5202471&lon=-122.674194&appid=b46251243caf85f31844ccabb8be9891
