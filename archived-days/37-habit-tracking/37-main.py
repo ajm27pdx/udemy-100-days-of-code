9e5be709ffaaefb9b0461748131f401e5d7fbd9c
Adam Martinez <ajm27@pdx.edu>
2023-06-27 14:07:19 -0700

    Finished day 38 with auth token

--- archived-days/37-habit-tracking/37-main.py
+++ archived-days/37-habit-tracking/37-main.py
@@ -2,40 +2,54 @@ import requests
 from datetime import datetime
 from auth import TOKEN
 USERNAME = 'ajm27pdx'
-GRAPH_ID = 'graph1'
+GRAPH_ID = 'graph2'
 pixela_endpoint = 'https://pixe.la/v1/users'
 
-user_params = {
-    'token': TOKEN,
-    'username': USERNAME,
-    'agreeTermsOfService': 'yes',
-    'notMinor': 'yes'
-}
-
-# response = requests.post(url=pixela_endpoint, json=user_params)
-# print(response.text)
-
-graph_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs'
 headers = {
     'X-USER-TOKEN': TOKEN
 }
-graph_config = {
-    'id': GRAPH_ID,
-    'name': 'Coding Graph',
-    'unit': 'min',
-    'type': 'int',
-    'color': 'ajisai'
-}
+
+# # Create User
+# user_params = {
+#     'token': TOKEN,
+#     'username': USERNAME,
+#     'agreeTermsOfService': 'yes',
+#     'notMinor': 'yes'
+# }
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
 # # Create Graph
-# requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+graph_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs'
+
+
+# # Create Graph
+# graph_config = {
+#     'name': 'OreSatTime',
+#     'unit': 'hrs',
+#     'type': 'int',
+#     'color': 'kuro'
+# }
+# response = requests.put(url=f'{graph_endpoint}/{GRAPH_ID}', json=graph_config, headers=headers)
+# print(response.text)
+
 
 date = f'{datetime.today().strftime("%Y%m%d")}'
 
-graph_update = {
-    'quantity': '45'
+# # Add Pixel
+new_pixel = {
+    'date': date,
+    'quantity': '1',
+    'optionalData': '{\"task\":\"attended meeting\"}'
 }
-
-
-response = requests.put(url=f'{graph_endpoint}/{GRAPH_ID}/{date}', headers=headers, json=graph_update)
+response = requests.post(url=f'{graph_endpoint}/{GRAPH_ID}', headers=headers, json=new_pixel)
 print(response.text)
+
+
+# # Update Pixel
+# graph_update = {
+#     'quantity': '45'
+# }
+# response = requests.put(url=f'{graph_endpoint}/{GRAPH_ID}/{date}', headers=headers, json=graph_update)
+# print(response.text)
 
