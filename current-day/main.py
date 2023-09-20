bec72a06e41b523039c37b2d071ac40b744825c4
Adam Martinez <ajm27@pdx.edu>
2023-09-20 14:08:39 -0700

    Completed day 60

---
+++ current-day/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=["POST"])
+def receive_data():
+    print(request.form)
+    return 'logged in'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
